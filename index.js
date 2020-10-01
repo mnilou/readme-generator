563c265dbe1164b9cd4153f93e885415048fa578
Nilou Mostofi <nilou@km-direct.com>
2020-09-30 18:19:14 -0700

    added questions in index.js

--- index.js
+++ index.js
@@ -14,56 +14,80 @@ const path = require("path");
 // Questions
 // Ba
 const questions = [
-            {
-          type: "input",
-          name: "title",
-          message: "What is the title of your project?"
-        },
-        {
-          type: "input",
-          message: "What languages do you know?",
-          name: "stack",
-          choices: [
-            "HTML", 
-            "CSS", 
-            "JavaScript", 
-            "MySQL"
-          ]
-        },
-        {
-          type: "list",
-          message: "What is your project license under?",
-          name: "license",
-          choices: [
-            {
-                name: "MIT",
-                value: "MIT"
-            },
-            {
-                name: "Apache 2.0",
-                value: "Apache2.0"
-            },
-            {
-                name: "GPL",
-                value: "GPL"
-            },
-            {
-                name: "ISC",
-                value: "ISC"
-            }
-     ]
-    }
-]
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "What is the description of your project?",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "What are the installation instructions?",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "What is the project usage?",
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "Who are the contributing parties?",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "What are the project tests?",
+  },
+  {
+    type: "checkbox",
+    message: "What is your project license under?",
+    name: "license",
+    choices: [
+      {
+        name: "MIT",
+        value: "MIT",
+      },
+      {
+        name: "Apache 2.0",
+        value: "Apache2.0",
+      },
+      {
+        name: "GPL",
+        value: "GPL",
+      },
+      {
+        name: "ISC",
+        value: "ISC",
+      },
+    ],
+  },
+//     {
+//     type: "input",
+//     name: "username",
+//     message: "What is your github user name?",
+//   },
+//   {
+//     type: "input",
+//     name: "repo",
+//     message: "What is your repo link?",
+//   },
+];
 // function to write README file
 function writeToFile(fileName, data) {
-    return fs.writeFileSync(path.join(process.cwd(), fileName), data)
+  return fs.writeFileSync(path.join(process.cwd, fileName), data);
 }
 
 // function to initialize program
 function init() {
-inquirer.prompt(questions).then(function(answers){
-writeToFile("README.md", generateMarkdown(answers));
-})
+  inquirer.prompt(questions).then(function (answers) {
+    writeToFile("README.md", generateMarkdown(answers));
+  });
 }
 
 // function call to initialize program
